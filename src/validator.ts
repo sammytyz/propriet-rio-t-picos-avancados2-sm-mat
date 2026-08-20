c2b01e74c5df3f79268fff8b3d568a88f4d187bb
SAMMY ABDALLA <sammy.abdalla@cs.cruzeirodosul.edu.br>
2026-08-20 10:11:35 -0300

    initial

---
+++ src/validator.ts
@@ -0,0 +1,10 @@
+function validarChamado(titulo: string): boolean {
+    if (!titulo || titulo.trim() === "") {
+        throw new Error("O título do chamado não pode ser vazio.");
+    }
+
+    return true;
+}
+
+export default validarChamado;
+
