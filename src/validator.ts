81d9b8ec78eedcf06726f9569ca9cecf4ccb24ec
Sammy Abdalla <sammyabdalla7@gmail.com>
2026-08-22 23:05:17 -0300

    Atualiza validação de títulos

--- src/validator.ts
+++ src/validator.ts
@@ -3,8 +3,15 @@ function validarChamado(titulo: string): boolean {
         throw new Error("O título do chamado não pode ser vazio.");
     }
 
+    if (titulo.length < 5) {
+        throw new Error("O título do chamado deve possuir no mínimo 5 caracteres.");
+    }
+
+    if (titulo.length > 100) {
+        throw new Error("O título do chamado deve possuir no máximo 100 caracteres.");
+    }
+
     return true;
 }
 
 export default validarChamado;
-
